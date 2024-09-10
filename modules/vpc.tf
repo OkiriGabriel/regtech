03c1292b86db456976b1cc9f99fe23518604ee8d
Gabriel Okiri <okirigabriel24@gmail.com>
2024-09-10 19:33:42 +0100

    completed task

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -111,16 +111,3 @@ resource "aws_route_table_association" "private" {
 data "aws_availability_zones" "available" {
   state = "available"
 }
-
-# Outputs
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
